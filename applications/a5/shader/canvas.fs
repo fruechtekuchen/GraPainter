e99544d45ccd90e2000e5e2fd93e2fab78207575
Laurin <laurin.hoppe@fau.de>
2026-02-11 19:30:44 +0100

    canvas shader

---
+++ applications/a5/shader/canvas.fs
@@ -0,0 +1,17 @@
+#version 430
+in vec4 pos_wc;
+in vec3 norm_wc;
+in vec2 tc;
+layout(location = 0) out vec4 out_col;
+layout(location = 1) out vec3 out_pos;
+layout(location = 2) out vec3 out_norm;
+
+uniform sampler2D diffuse;
+
+
+void main() {
+    vec4 diff = texture(diffuse, tc);
+    out_col = diff;
+    out_pos = pos_wc.xyz;
+    out_norm = norm_wc;
+}
